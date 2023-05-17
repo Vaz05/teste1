79495229637ce023818c0a9de51880507d99163d
Vaz05 <diogo.vaz@academiadecodigo.org>
2023-05-17 17:39:29 +0100

    Testing inputs

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import Table from './Table'
 import MyForm from './Form'
 
 class App extends Component {
--- react-tutorial/src/Form.test.js
+++ react-tutorial/src/Form.test.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import { render, screen} from '@testing-library/react';
+import {render} from 'react-dom';
 import MyForm from './Form';
-import '@testing-library/jest-dom';
 
 describe('MyForm', () => {
   Object.defineProperty(window, 'matchMedia', {
@@ -18,16 +16,25 @@ describe('MyForm', () => {
     })),
   });
 
-    it('renders the name input', () => {
-      render(<MyForm />);
-      const nameInput = screen.getByLabelText('Name');
-      expect(nameInput).toBeInTheDocument();
+  it("test", () => {
+    expect(true).toBe(true)
+  })
 
-    });
   
-    it('renders the job input', () => {
-      render(<MyForm />);
-      const jobInput = screen.getByLabelText('Job');
-      expect(jobInput).toBeInTheDocument();
-    });
+  it('renders the name input', () => {
+    document.addEventListener('DOMContentLoaded', () => {
+      render(<MyForm />, document.getElementById('root'));
+      const nameInput = getByLabelText('Name');
+      expect(nameInput).toBeInTheDocument();
+    }); 
+  });
+
+  it('renders the job input', () => {
+    document.addEventListener('DOMContentLoaded', () => {
+      render(<MyForm />, document.getElementById('root'));
+      const jobInput = getByLabelText('Job');
+      enxpect(jobInput).toBeInTheDocument();
+    });  
+  });
+
 });
